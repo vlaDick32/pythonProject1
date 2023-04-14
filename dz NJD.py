649171a1f9fc3cfa8c31850500c38643c4927127
kayut <kayutovvlad@gmail.com>
2023-04-14 17:45:21 +0200

    new file

--- dz NJD.py
+++ dz NJD.py
@@ -1,19 +1,23 @@
-import logging
-
 result = []
 def divider(a, b):
- if a < b:
-  raise ValueError
- if b > 100:
- raise IndexError
-  return a/b
-try:
-   print(10/0)
-except Exception:
- logging.exception('Exceptiong')
+try :
+   if not isinstance(a,(int, float)) or not isinstance(b, (int, float))
+        raise TypeError('Invalid for divider ()')
+   if b==0:
+       raise ZeroDivisionError('ZeroDivisiob eror')
+   if a < b:
+        raise ValueError('value eror')
+   if b > 100:
+  raise IndexError ('b>100 eror Index' )
+ except (TypeError, ZeroDivisionError,ValueError,IndexError) as r:
+ print(f f'Exception raised: {type(r).__name__}')
+return None
+return a/b
+
    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 for key in data:
  res = divider(key, data[kem])
+if res is note None
  result.append(res)
 
 print(result)
